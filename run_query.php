52c54c4cd98efc63ead1af4cee2f33f6129193b8
rojinadeuja <rojinadeuja33g@gmail.com>
2020-04-17 20:10:26 -0500

    Added form post

--- run_query.php
+++ run_query.php
@@ -1,5 +1,12 @@
 <?php
-echo 'Run Query';
+// Check if the form is submitted 
+if ( isset( $_POST['submit'] ) ) { 
+// retrieve the form data by using the element's name attributes value as key 
+$query = $_POST['query']; 
+echo 'The query you entered is ' . $query; 
+exit; 
+} 
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
