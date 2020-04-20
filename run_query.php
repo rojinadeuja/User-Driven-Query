e9daf1ebab86ad9c3365d509b098bc99d4a74eaf
rojinadeuja <rojinadeuja33g@gmail.com>
2020-04-20 14:59:09 -0500

    Added show results from query on test db

--- run_query.php
+++ run_query.php
@@ -1,16 +1,39 @@
 <?php
 // Check if the form is submitted 
 if ( isset( $_POST['submit'] ) ) { 
-// retrieve the form data by using the element's name attributes value as key 
-$query = $_POST['query']; 
-echo 'The query you entered is ' . $query; 
-exit; 
-} 
+    // retrieve the form data by using the element's name attributes value as key 
+    $query = $_POST['query']; 
+    echo 'The query you entered is : <br/>';
+    echo $query; 
+    echo '<br/>';
+    echo '<br/>The results are : <br/>';
+    $username = "mytestuser"; 
+    $password = "123";   
+    $host = "localhost";
+    $database= "mytestdb";
+    
+    $server = mysqli_connect($host, $username, $password);
+    $connection = mysqli_select_db($server, $database);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $myquery = $query; //"SELECT  `date_added`, `close` FROM  price";
+    $query = mysqli_query($server, $myquery);
+    
+    if ( ! $query ) {
+        echo ('Error!');
+        echo mysqli_error();
+        die;
+    }
+    
+    $data = array();
+    
+    for ($x = 0; $x < mysqli_num_rows($query); $x++) {
+        $data[] = mysqli_fetch_assoc($query);
+    }
+    
+    echo json_encode($data);     
+     
+    mysqli_close($server);
+exit;
+}
 ?>
 
